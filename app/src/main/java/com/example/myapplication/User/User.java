110634e616f546b16d755272f91ea0ad7002ed12
alstjd517 <alstjd517@kunsan.ac.kr>
2019-06-05 23:23:08 +0900

    User getter/setter

---
+++ app/src/main/java/com/example/myapplication/User/User.java
@@ -0,0 +1,49 @@
+package com.example.myapplication.User;
+
+public class User {
+    String id;
+    String passwd;
+    String name;
+    String num;
+    String gender;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNum() {
+        return num;
+    }
+
+    public void setNum(String num) {
+        this.num = num;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+}
