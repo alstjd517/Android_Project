36b0b7f31628c60d73ee97ce229fe71091fe824e
axd10 <axd10@kunsan.ac.kr>
2019-06-09 02:14:28 +0900

    게시판 상세보기 (디비)

---
+++ app/src/main/java/com/example/myapplication/BoSt.java
@@ -0,0 +1,36 @@
+package com.example.myapplication;
+
+
+
+public class BoSt {
+    private String member_id; // 프로필사진
+
+    public String getMember_id() {
+        return member_id;
+    }
+
+    public void setMember_id(String member_id) {
+        this.member_id = member_id;
+    }
+
+    public String getMember_name() {
+        return member_name;
+    }
+
+    public void setMember_name(String member_name) {
+        this.member_name = member_name;
+    }
+
+    public String getMember_address() {
+        return member_address;
+    }
+
+    public void setMember_address(String member_address) {
+        this.member_address = member_address;
+    }
+
+    private String member_name; // 친구 정보
+    private String member_address; // 핸드폰 번호
+
+
+}
