112951849eb38b994b9b10955a031bdb8d980cfd
axd10 <axd10@kunsan.ac.kr>
2019-06-10 03:23:30 +0900

    유사도기능

--- app/src/main/java/com/example/myapplication/Sim/resultST.java
+++ app/src/main/java/com/example/myapplication/Sim/resultST.java
@@ -4,6 +4,17 @@ package com.example.myapplication.Sim;
 public class resultST implements Comparable<resultST>{
     private  String name;
     private  double measure;
+  /*  public resultST() {
+        name = "";
+
+        measure = 0;
+    }
+
+    public resultST(String name, double measure) {
+        this.name = name;
+
+        this.measure = measure;
+    }*/
 
     public String getName() {
         return name;
@@ -26,13 +37,14 @@ public class resultST implements Comparable<resultST>{
         double aa =this.measure;
         double bb =f.measure;
 
-        if(  aa >  bb ) {
+        if(  aa <  bb ) {
             return -1;
-        }else if(aa < bb) {
-            return -1;
-        }else {
+        }else if(aa == bb) {
             return 0;
+        }else {
+            return 1;
         }
     }
+
 
 }
